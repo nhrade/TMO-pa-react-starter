8251774e1d33df3edb1fc5d5e8db3eac399dbb92
Noah Hradek <Noah.Hradek1@T-Mobile.com>
2022-05-04 11:07:32 -0600

    files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import AddRecipe from './AddRecipe';
 import RecipeList from './RecipeList';
 
 function App() {
 
   const [recipes, setRecipes] = useState([]);
-
-  let recipeList = null;
-
-
 
   return (
     <div>
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,5 +1,5 @@
 
-export default function({ recipes, setRecipes }) {
+export default function RecipeList({ recipes, setRecipes }) {
     console.log(recipes);
     if(recipes.length <= 0) {
         return <p>There are no recipes to list</p>
