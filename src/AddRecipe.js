b1c8eba76df73b1aab8b0bb8f0d3cbf4d3d34484
Noah Hradek <Noah.Hradek1@T-Mobile.com>
2022-05-04 11:38:12 -0600

    changed files

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -17,14 +17,14 @@ export default function AddRecipe({ recipes, setRecipes }) {
     if(showAddRecipe) {
         return (
             <div>
-                <label>recipe-name:</label><input value={recipeName} onChange={e => setRecipeName(e.target.value)} type="text"></input>
+                <label>recipe-name:</label><input name="recipe-name" value={recipeName} onChange={e => setRecipeName(e.target.value)} type="text"></input>
                 <br></br>
-                <label>recipe-instructions:</label><input value={recipeInstructions} onChange={e => setRecipeInstructions(e.target.value)} type="text"></input>
+                <label>recipe-instructions:</label><input name="recipe-instructions" value={recipeInstructions} onChange={e => setRecipeInstructions(e.target.value)} type="text"></input>
                 <button onClick={submitRecipe}>Submit</button>
             </div>
         )
     }
     else {
-        return <button onClick={addRecipe}>Add Recipe</button>
+        return <button onClick={addRecipe}>add recipe</button>
     }
 }
