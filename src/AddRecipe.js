3ee31b761a1bf824092693751cd9a234f00912b1
Noah Hradek <Noah.Hradek1@T-Mobile.com>
2022-05-05 10:29:08 -0600

    fixed role

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -16,11 +16,11 @@ export default function AddRecipe({ recipes, setRecipes }) {
     
     if(showAddRecipe) {
         return (
-            <div>
+            <div> 
                 <form>
-                    <label>recipe-name:</label><input name="recipe-name" value={recipeName} onChange={e => setRecipeName(e.target.value)} type="text"></input>
+                    <label>recipe-name:</label><input role="textbox" name="recipe-name" value={recipeName} onChange={e => setRecipeName(e.target.value)} type="text"></input>
                     <br></br>
-                    <label>recipe-instructions:</label><input name="recipe-instructions" value={recipeInstructions} onChange={e => setRecipeInstructions(e.target.value)} type="text"></input>
+                    <label>recipe-instructions:</label><input role="textbox" name="recipe-instructions" value={recipeInstructions} onChange={e => setRecipeInstructions(e.target.value)} type="text"></input>
                 </form>  
                 <button onClick={submitRecipe}>Submit</button>
             </div>
